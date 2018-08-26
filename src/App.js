c9276163247bf16200c631aa1d82ae76e56a7f86
Azez Nassar <azez.nassar@yahoo.com>
2018-08-26 18:47:50 +1200

    configure react router for gh pages

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.scss';
 class App extends React.Component {
     render(){
         return (
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <div>
                     <Header />
 
